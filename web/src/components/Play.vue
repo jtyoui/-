ee0362da452fc1ccc52e6e302934868901f8f3da
张伟 <jtyoui@qq.com>
2021-12-09 09:34:38 +0800

    前端架构基本完成

--- web/src/components/Play.vue
+++ web/src/components/Play.vue
@@ -9,23 +9,30 @@
         </el-icon>
         <span class="tittle">&emsp;我和僵尸有个约会1——马小玲爱上我</span>
         <span class="start">
-        <el-icon :size="25"><star/></el-icon>
+        <el-icon :size="24"><star/></el-icon>
         <span> 20</span>
       </span>
       </div>
     </div>
     <div class="box">
-      <el-menu>
-        <el-sub-menu>
+      <el-menu :default-openeds="[0]">
+        <el-sub-menu :index="0">
           <template #title>
             <el-icon>
               <Menu/>
             </el-icon>
             <span>我和僵尸有个约会一</span>
           </template>
-          <div class="infinite-list" style="overflow: auto">
+          <div class="list" style="overflow: auto">
             <div v-for="i in 20" :key="i" class="infinite-list-item">
-              <el-menu-item :index="1-i">P{{ i }} 我和僵尸有个约会{{ i }}<span class="time">50:00</span></el-menu-item>
+              <el-menu-item :index="i">
+                <el-tooltip :content="'P'+i+'&emsp;我和僵尸有我和僵尸有个约会个约会'+i" placement="top" show-after="800">
+                  <div class="name">
+                    P{{ i }}&emsp;我和僵尸有我和僵尸有个约会个约会{{ i }}
+                  </div>
+                </el-tooltip>
+                <div class="time">50:00</div>
+              </el-menu-item>
             </div>
           </div>
         </el-sub-menu>
@@ -41,7 +48,7 @@ import {Menu} from '@element-plus/icons'
 
 const options = reactive({
   width: "984px",
-  height: "777px",
+  height: "700px",
   title: "我和僵尸有个约会开头",
   src: "https://jtyoui.oss-accelerate-overseas.aliyuncs.com/video/login.mp4",
   muted: false,
@@ -55,7 +62,6 @@ const options = reactive({
 
 <style scoped>
 .video {
-  width: 1000px;
   margin-left: 20%;
 }
 
@@ -81,20 +87,20 @@ const options = reactive({
 }
 
 .time {
+  width: 20px;
   margin-left: 30px;
 }
 
-.infinite-list {
-  height: 770px;
+.list {
+  height: 700px;
   list-style: none;
+  width: 100%;
 }
 
-.infinite-list-item {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 50px;
-  background: var(--el-color-primary-light-9);
-  color: var(--el-color-primary);
+.name {
+  width: 200px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
 </style>
